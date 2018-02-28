e7f95f1e5b50e3ba018295697df9061047efa4d0
johneckert <johnteckert@gmail.com>
2018-02-28 17:10:30 -0500

    data seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+userOne = User.create(name: 'John', hiScore:0, hiLevel: 1)
+userTwo = User.create(name: 'Andrea', hiScore: 0, hiLevel: 1)
+
+themeOne = Theme.create(colorOne: 'red', colorTwo: 'yellow', colorThree: 'blue', colorFour: 'green', name: 'Classic', background: 'white')
+
+levelOne = GameBoard.create(level: 1, maxScore: 100)
+levelTwo = GameBoard.create(level: 2, maxScore: 200)
+levelThree = GameBoard.create(level: 3, maxScore: 300)
