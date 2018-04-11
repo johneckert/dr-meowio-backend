70d8b85b242db32d9944489ac2f92a7a78294eca
johneckert <johnteckert@gmail.com>
2018-04-11 14:39:31 -0400

    broken gameovergit add .git add .

--- app/controllers/highscores_controller.rb
+++ app/controllers/highscores_controller.rb
@@ -1,7 +1,23 @@
 class HighscoresController < ApplicationController
   def index
-    highscores = Highscore.all
-    top_ten = highscores.sort {|a, b| b.score <=> a.score}.first(10)
+    top_ten = calculate_top_ten
     render json: top_ten, status: 200
   end
+
+  def create
+    new_score = Highscore.create(score: params[:score], initials: params[:initials])
+    top_ten = calculate_top_ten
+    render json: top_ten, status: 200
+
+  end
+
+  private
+
+
+
+  def calculate_top_ten
+    highscores = Highscore.all
+    top_ten = highscores.sort {|a, b| b.score <=> a.score}.first(10)
+  end
+
 end
